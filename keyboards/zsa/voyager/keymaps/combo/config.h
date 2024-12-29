b48b4ca3f5ee5d69b347fb8c0433e822c1c4fbc3
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2024-12-29 21:53:45 +0100

    complete double layout

--- keyboards/zsa/voyager/keymaps/combo/config.h
+++ keyboards/zsa/voyager/keymaps/combo/config.h
@@ -1,2 +1,4 @@
 #define COMBO_TERM 40
-#define TAPPING_TERM 100
+#define TAPPING_TERM 150
+
+#define TAPPING_TOGGLE 2 // TT(layer) required taps
--- keyboards/zsa/voyager/keymaps/combo/keymap.c
+++ keyboards/zsa/voyager/keymaps/combo/keymap.c
@@ -24,12 +24,27 @@ combo_t key_combos[] = {
     COMBO(jkl_combo, RSFT(KC_RGUI)),
 };
 
+// CW_TOGG - Caps Word
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        _______, KC_1,  KC_2,  KC_3,  KC_4,  KC_5,      KC_6,        KC_7,      KC_8,     KC_9,    KC_0,     _______,
-        CW_TOGG, KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,      KC_Y,        KC_U,      KC_I,     KC_O,    KC_P,     _______,
-        _______, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,      KC_H,        KC_J,      KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
-        _______, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,      KC_N,        KC_M,      KC_COMM,  KC_DOT,  KC_SLSH,  KC_BSPC,
-                                             KC_ESCAPE, KC_SPACE,    KC_SPACE,  KC_TAB
+        KC_CAPS,  _______,  _______,  _______,  _______,  _______,      _______,  _______,  _______,  _______,  _______,  _______,
+        CW_TOGG,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     _______,
+        KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,         KC_H,     KC_J,     KC_K,     KC_L,     KC_ENTER, KC_BSPC,
+        _______,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,         KC_N,     KC_M,     KC_RIGHT, KC_UP,    KC_DOWN,  KC_LEFT,
+                                                TT(1),    KC_SPACE,     KC_SPACE, TT(1)
+    ),
+    [1] = LAYOUT(
+        _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,  _______,  _______,  _______,  _______,
+        KC_BSLS,  KC_SLSH,  KC_DQT,   KC_COLN,  KC_LPRN,  KC_LBRC,      KC_LCBR,  KC_COMM,  KC_DOT,   KC_UNDS,  KC_EQUAL, KC_GRAVE,
+        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,         KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSPC,
+        KC_PIPE,  KC_QUES,  KC_QUOTE, KC_SCLN,  KC_RPRN,  KC_RBRC,      KC_RCBR,  KC_LT,    KC_GT,    KC_MINUS, KC_PLUS,  KC_TILDE,
+                                                TO(0),    KC_LSFT,      KC_RSFT,  KC_TAB
     )
+    // [2] = LAYOUT(
+    //     _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______, _______, _______,  _______,  _______,
+    //     _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______, _______, _______,  _______,  _______,
+    //     _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______, _______, _______,  _______,  _______,
+    //     _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______, _______, _______,  _______,  _______,
+    //                                             TO(0),    KC_LSFT,      KC_RSFT,  KC_TAB
+    // )
 };
