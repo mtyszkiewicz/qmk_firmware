b3fc086de4c4ee65a91c91e1d0388b9565726d30
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2025-06-18 15:17:24 +0200

    add gaming layout

--- keyboards/zsa/voyager/keymaps/mtyszkiewicz/keymap.c
+++ keyboards/zsa/voyager/keymaps/mtyszkiewicz/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM combo_right_tmux_prefix[] = {KC_J, KC_SPACE, COMBO_END};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_CAPS,  _______,  _______,  _______,  _______,  _______,      _______,  _______,  _______,  _______,  _______,  _______,
+        KC_CAPS,  _______,  _______,  _______,  _______,  _______,      _______,  _______,  _______,  _______,  _______,  TO(2),
         CW_TOGG,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     _______,
         KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,         KC_H,     KC_J,     KC_K,     KC_L,     KC_ENTER, KC_BSPC,
         KC_TAB,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,         KC_N,     KC_M,     KC_RIGHT, KC_UP,    KC_DOWN,  KC_LEFT,
@@ -31,19 +31,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_PIPE,  KC_QUES,  KC_QUOTE, KC_SCLN,  KC_RPRN,  KC_RBRC,      KC_RCBR,  KC_LT,    KC_GT,    KC_MINUS, KC_PLUS,  KC_TILDE,
                                                 TO(0),    KC_LSFT,      KC_RSFT,  KC_TAB
     ),
+    // Windows gaming:
+    // - Left keybaord part is shifted 1 column right (better WASD access)
+    // - CAPS switched with Shift
+    // - Right space replaced by Win key
     [2] = LAYOUT(
-        _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  _______,  KC_G,     KC_W,     KC_E,     _______,      _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  KC_LSFT,  KC_A,     KC_S,     KC_D,     KC_R,         _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  KC_LCTL,  KC_LALT,  KC_C,     KC_V,     KC_SPACE,     _______,  _______,  _______,  _______,  _______,  _______,
-                                                TO(0),    KC_SPACE,     _______,  _______
-    ),
-    [3] = LAYOUT(
-        _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______, _______, _______,  _______,  _______,
-        _______,  _______,  _______,  KC_W,     _______,  _______,      _______,  _______, _______, _______,  _______,  _______,
-        _______,  KC_LSFT,  KC_A,     KC_S,     KC_D,     _______,      _______,  _______, _______, _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______, _______, _______,  _______,  _______,
-                                                TO(0),    KC_SPACE,     _______,  _______
+        KC_ESC,   KC_0,     KC_1,     KC_2,     KC_3,     KC_4,         KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     TO(0),
+        KC_T,     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
+        KC_G,     KC_LSFT,  KC_A,     KC_S,     KC_D,     KC_F,         KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_ENTER,
+        KC_B,     KC_CAPS,  KC_Z,     KC_X,     KC_C,     KC_V,         KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+                                                KC_LCTL,  KC_SPACE,     KC_RGUI,  KC_LALT
     )
 };
 
